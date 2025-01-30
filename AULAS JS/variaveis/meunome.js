20d6296246868517bc012599f37fbcebc33c7c85
Rian Reuber <rianreuberfaustino@hotmail.com>
2025-01-30 15:29:31 -0300

    modified:   AULAS JS/variaveis/meunome.js 	new file:   AULAS JS/variaveis/pagamento.js

--- AULAS JS/variaveis/meunome.js
+++ AULAS JS/variaveis/meunome.js
@@ -3,14 +3,12 @@ function escrevaMeuNome(nome) {
     return nome = "Rian";
 }
 
-
-
 function verificarIdade(idade) {
     if (idade <= 17)
         return escrevaMeuNome() + ' é menor de idade';
     else if (idade >= 18 && idade <= 119)
-        return 'Maior de idade';
+        return  escrevaMeuNome() + ' é maior de idade';
     else (idade < 0 && idade > 120)
     return 'Idade inválida';
 }
-console.log(verificarIdade(16));
+console.log(verificarIdade(18));
---
+++ AULAS JS/variaveis/pagamento.js
@@ -0,0 +1,42 @@
+/*Calculo de desconto no pagamento */
+
+
+function aplicarDesconto(valor, desconto) {
+    return valor - (valor * desconto / 100);
+}
+
+function aplicarJuros(valor, juros) {
+    return valor + (valor * juros / 100);
+}
+
+const valorProduto = 200;
+const qtdParcelas = 3;
+const formaPagamento = 'credito';
+/* Formas de pagamento: dinheiro, pix, credito, debito */
+
+if (formaPagamento === 'debito') {
+    console.log('O valor a ser pago no débito é:', aplicarDesconto(valorProduto, 10));
+}
+else if (formaPagamento === 'dinheiro') {
+    console.log('O valor a ser pago no dinheiro é:', aplicarDesconto(valorProduto, 15));
+}
+else if (formaPagamento === 'pix') {
+    console.log('O valor a ser pago no PIX é:', aplicarDesconto(valorProduto, 15));
+}
+else if (formaPagamento === 'credito' && qtdParcelas <= 2) {
+    console.log('O valor a ser pago no crédito é:', valorProduto.toFixed(2));
+}
+else if (formaPagamento === 'credito' && qtdParcelas > 2) {
+    console.log('O valor a ser pago é:', aplicarJuros(valorProduto, 10).toFixed(2));
+}
+else if (formaPagamento !== 'debito' && formaPagamento !== 'dinheiro' && formaPagamento !== 'pix' && formaPagamento !== 'credito') {
+    console.log('Forma de pagamento inválida');
+}
+
+
+/*Calculo de desconto no pagamento */
+
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrecimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrecimo;
+}
