11dc6b715e949f05f72691a518cbf7d05afdbbae
Rian Reuber <rianreuberfaustino@hotmail.com>
2025-02-13 17:47:28 -0300

    new file:   AULAS JS/variaveis/carros.js 	modified:   AULAS JS/variaveis/pessoa.js 	new file:   AULAS JS/variaveis/praticaPessoaObjeto.js 	new file:   brainstorm.txt 	modified:   infolink.html 	new file:   linkedin.svg 	new file:   somosum.txt

---
+++ AULAS JS/variaveis/carros.js
@@ -0,0 +1,16 @@
+class Carro {
+    constructor(marca, cor, mediaKMporLitro) {
+        this.marca = marca;
+        this.cor = cor;
+        this.mediaKMporLitro = mediaKMporLitro;
+        this.valorLitroCombustivel = 15.29;
+        this.distanciaViagem = 100;
+    }
+
+    calcularConsumo() {
+        console.log(`Você consumirá com o C ${this.marca} o valor de R$ ${(this.distanciaViagem / this.mediaKMporLitro * this.valorLitroCombustivel).toFixed(2)} nesta viagem de ${this.distanciaViagem} km`);
+    }
+
+}
+const carro1 = new Carro('Fiat toro', 'Vermelho', 30);
+carro1.calcularConsumo();
--- AULAS JS/variaveis/pessoa.js
+++ AULAS JS/variaveis/pessoa.js
@@ -6,14 +6,15 @@ class Pessoa {
     constructor(nome, idade) {
         this.nome = nome;
         this.idade = idade;
-        this.anoDeNascimento = 2025 - idade;
+        this.anoDeNascimento = new Date().getFullYear() - idade;
     }
 
     descrever() {
-        console.log(`Meu nome é ${this.nome}, e minha idade é ${this.idade} `);
+        console.log(`Meu nome é ${this.nome}, e minha idade é ${this.idade} e nasci em ${this.anoDeNascimento}`);
     }
 }
 
+
 const rian = new Pessoa('Rian', 24);
-console.log(rian);  // Exibe o objeto no console
-rian.descrever();   // Agora o método é chamado e exibe a mensagem
+//console.log(rian);  // Exibe o objeto
+rian.descrever();   // Chama o método para exibir a mensagem
