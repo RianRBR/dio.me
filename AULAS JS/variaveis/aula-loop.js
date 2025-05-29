4ce7fe5aa8e64549abff525a956331e81422002c
Rian Reuber <rianreuberfaustino@hotmail.com>
2025-05-29 17:59:17 -0300

    new file:   AULAS JS/variaveis/aula-loop.js 	new file:   facul.html

---
+++ AULAS JS/variaveis/aula-loop.js
@@ -0,0 +1,40 @@
+/*const nome = 'Rian Reuber Faustino da Silva';
+
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+}*/
+
+/* const notas = [];
+
+notas.push(5);
+notas.push(10);
+notas.push(10);
+
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const notaFinal = notas[i];
+    soma = soma + notaFinal;
+    
+}
+const media = (soma / notas.length).toFixed(2);
+console.log(media);*/
+
+/* AQUI COMEÇA A TABUADA 
+
+for (let i = 1; i <= 10 ; i++) {
+    let numero = 5; // número que será multiplicado
+    const resultado = i * numero; // resultado da multiplicação
+    console.log(`${numero} x ${i} = ${resultado}`); // exibe o resultado
+   
+    
+}*/
+
+// AQUI VEREMOS SE É PAR OU IMPAR
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} é par`);
+    }
+}
